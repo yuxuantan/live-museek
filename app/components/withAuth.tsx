4ee8f83d6648b311055f1d908734e8ac75838685
Jace Tan <jace.tan>
2024-06-17 13:43:23 +0800

    remove with auth

--- app/components/withAuth.tsx
+++ app/components/withAuth.tsx
@@ -1,22 +1,22 @@
-// src/components/withAuth.tsx
-import React from 'react';
-import { useAuth } from '../context/AuthContext';
-import { useRouter } from 'next/router';
+// // src/components/withAuth.tsx
+// import React from 'react';
+// import { useAuth } from '../context/AuthContext';
+// import { useRouter } from 'next/router';
 
-const withAuth = <P extends object>(Component: React.ComponentType<P>): React.FC<P> => {
-  return (props: P) => {
-    const { user } = useAuth();
-    const router = useRouter();
+// const withAuth = <P extends object>(Component: React.ComponentType<P>): React.FC<P> => {
+//   return (props: P) => {
+//     const { user } = useAuth();
+//     const router = useRouter();
 
-    if (!user) {
-      if (typeof window !== 'undefined') {
-        router.replace('/login'); // Redirect to login page if not authenticated
-      }
-      return null;
-    }
+//     if (!user) {
+//       if (typeof window !== 'undefined') {
+//         router.replace('/login'); // Redirect to login page if not authenticated
+//       }
+//       return null;
+//     }
 
-    return <Component {...props} />;
-  };
-};
+//     return <Component {...props} />;
+//   };
+// };
 
-export default withAuth;
+// export default withAuth;
