31aeca68ea8691429ff7e998d08e27546fcfac7a
Jace Tan <jace.tan>
2024-07-16 08:55:01 +0800

    remove scrape eb page to prevent deployment issues

--- app/(admin)/scrape-eb/page.tsx
+++ app/(admin)/scrape-eb/page.tsx
@@ -1,26 +1,26 @@
-'use client'
-import React from 'react';
+// 'use client'
+// import React from 'react';
 
-const Page: React.FC = () => {
-    const handleScrapeData = async () => {
-        try {
-            const response = await fetch('/api/scrape-eventbrite');
-            const data = await response.json();
-            // Process the scraped data here
-            console.log(data);
-        } catch (error) {
-            console.error('Error scraping data:', error);
-        }
-    };
+// const Page: React.FC = () => {
+//     const handleScrapeData = async () => {
+//         try {
+//             const response = await fetch('/api/scrape-eventbrite');
+//             const data = await response.json();
+//             // Process the scraped data here
+//             console.log(data);
+//         } catch (error) {
+//             console.error('Error scraping data:', error);
+//         }
+//     };
 
-    return (
-        <div>
-            {/* Create button to scrape data */}
-            <button className="secondary-btn" onClick={handleScrapeData}>
-                Scrape Eventbrite Data
-            </button>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             {/* Create button to scrape data */}
+//             <button className="secondary-btn" onClick={handleScrapeData}>
+//                 Scrape Eventbrite Data
+//             </button>
+//         </div>
+//     );
+// };
 
-export default Page;
+// export default Page;
