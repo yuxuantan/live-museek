e88eb014ee647a75c63b62d725c96ddc492f0a50
Jace Tan <jace.tan>
2024-07-16 09:04:14 +0800

    remove api route

--- _archived/scrape-eventbrite/route.js
+++ _archived/scrape-eventbrite/route.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../../app/supabaseClient';
 
 export async function GET() {
     try {
