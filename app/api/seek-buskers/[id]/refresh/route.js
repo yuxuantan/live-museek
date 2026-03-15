87a52d7daaa606be403ac9349fda85e9e9385343
YX <yu_xuan_tan@hotmail.com>
2026-03-15 10:40:21 +0800

    fix max duration

--- app/api/seek-buskers/[id]/refresh/route.js
+++ app/api/seek-buskers/[id]/refresh/route.js
@@ -2,7 +2,7 @@ import { LocationRefreshError, refreshBuskerById } from '../../../../server/nac_
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function POST(request, { params }) {
   try {
--- app/api/seek-locations/[id]/refresh/route.js
+++ app/api/seek-locations/[id]/refresh/route.js
@@ -2,7 +2,7 @@ import { LocationRefreshError, refreshLocationById } from '../../../../server/na
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function POST(request, { params }) {
   try {
